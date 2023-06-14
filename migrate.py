74da03c5591795662124768699461ed64f5563cf
Jie Chen <jiechenmc@gmail.com>
2023-06-14 00:10:20 -0400

    set section as part of chats/{cls}

--- migrate.py
+++ migrate.py
@@ -24,7 +24,7 @@ for cls in class_offerings:
         section_data['_created'] = firestore.SERVER_TIMESTAMP
         section_data['_lastWiped'] = firestore.SERVER_TIMESTAMP
 
-        db.document(f'chats/{cls}').set(None)
+        db.document(f'chats/{cls}').set({'sections': list(class_data.keys())})
         db.collection(f'chats/{cls}/{sec}').document('meta').set(section_data)
 
         # members and messages will be found in the following paths
